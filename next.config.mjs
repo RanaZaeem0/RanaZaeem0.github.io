cc4d84d143aca74d4a65bdccc671b80394e2b817
Rana Zaeem <149497053+RanaZaeem0@users.noreply.github.com>
2025-09-06 13:57:15 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // ensures static HTML export
+  basePath: "/RanaZaeem0.github.io", // IMPORTANT if your repo is not <username>.github.io
+  images: {
+    unoptimized: true, // disable next/image optimization for static export
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
