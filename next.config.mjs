2c46801c7128c78f33587b25a801905847868a54
Rana Zaeem <149497053+RanaZaeem0@users.noreply.github.com>
2025-09-06 14:02:06 +0500

    udpate

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // ensures static HTML export
-  basePath: "/RanaZaeem0.github.io", // IMPORTANT if your repo is not <username>.github.io
-  images: {
-    unoptimized: true, // disable next/image optimization for static export
-  },
 };
 
 module.exports = nextConfig;
