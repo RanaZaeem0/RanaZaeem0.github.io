fe25512bc7d1457d262ec12f48122f3e52f0c13d
Rana Zaeem <149497053+RanaZaeem0@users.noreply.github.com>
2025-09-06 14:39:39 +0500

    update

--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { projects } from "./data";
+import Image from "next/image";
+import { avatarImg } from "../../public";
 
 function Work() {
+  const projects = [
+    { title: "LMS", iamge: "" },
+    {
+      title: "E-commerce",
+      iamge: "",
+    },
+    { title: "Portfolio", iamge: "" },
+    { title: "Blog", iamge: "" },
+    { title: "Social Media", iamge: "" },
+  ];
+
   return (
     <div>
       <div className=" flex justify-center items-center ">
         <h1 className="text-4xl font-bold">My Work</h1>
 
-        <div className=""></div>
+        <div className="bg-gray-200">
+          <div className="w-5/6 h-5/6 ">
+            <Image src={avatarImg} width={100} height={100} />
+          </div>
+        </div>
       </div>
     </div>
   );
